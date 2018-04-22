48548e88828854571282bdc79c53c3509c9fea39
Administrator <Administrator@Kwanloong-PC>
2018-04-22 19:10:41 +0800

    测试3

--- AppInfoSystem/src/cn/appsys/pojo/Hello.java
+++
@@ -1,7 +0,0 @@
-package cn.appsys.pojo;
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("test");
-	}
-}
--- AppInfoSystem/src/cn/appsys/pojo/User.java
+++
@@ -1,5 +0,0 @@
-package cn.appsys.pojo;
-
-public class User {
-
-}
